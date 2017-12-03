ff6e6b6b916893428eb14bbdf3721d4c7d49a15b
clrnd <welcometothechango@gmail.com>
2017-12-03 11:24:07 -0300

    handling cmdline options

--- src/main.cpp
+++ src/main.cpp
@@ -3,37 +3,91 @@
 #include <iostream>
 #include <array>
 #include <vector>
+#include <cstdlib>
+#include <cerrno>
+
+#include "tclap/CmdLine.h"
 #include "AudioFile.h"
 #include "MidiFile.h"
 
 using uchar = unsigned char;
 
-int main(void){
+struct Options {
+    std::string infile;
+    std::string outfile;
+    bool half;
+};
+
+Options parseOptions(int argc, char* argv[]){
+    TCLAP::CmdLine cmd("WAV to Blofeld SysEx", ' ', "0.1");
+
+    TCLAP::UnlabeledValueArg<std::string>
+        infile("infile", "Input WAV file.", true, "", "infile");
+    cmd.add(infile);
+
+    TCLAP::UnlabeledValueArg<std::string>
+        outfile("outfile", "Output midi file.", true, "", "outfile");
+    cmd.add(outfile);
+
+    TCLAP::SwitchArg
+        half("d", "double", "Use only half of the samples.", cmd, false);
+
+    cmd.parse(argc, argv);
+
+    return Options {
+        .infile = infile.getValue(),
+        .outfile = outfile.getValue(),
+        .half = half.getValue(),
+    };
+}
+
+
+int main(int argc, char* argv[]){
+    Options opts = parseOptions(argc, argv);
+
     AudioFile<double> audioFile;
-    audioFile.load("../Untitled.wav");
+
+    if (!audioFile.load(opts.infile)) {
+        exit(EXIT_FAILURE);
+    }
+
     audioFile.printSummary();
 
     int numSamples = audioFile.getNumSamplesPerChannel();
-    std::cout << "numSamples: " << numSamples << std::endl;
+    if (!opts.half and numSamples != 64*128) {
+        std::cerr << "File's first channel must have 64*128 = "
+                  << 64*128 << " samples!" << std::endl;
+        exit(EXIT_FAILURE);
+    } else if (opts.half and numSamples != 64*128*2) {
+        std::cerr << "File's first channel must have 64*256 = "
+                  << 64*128*2 << " samples!" << std::endl;
+        exit(EXIT_FAILURE);
+    }
 
     // 21 bit values => 1048575 to -1048575
     int max = 1048575;
     std::array<int, 64*128> samples;
 
     for (int i = 0; i < numSamples; i++) {
-        // between -1 and 1
-        double currentSample = audioFile.samples[0][i];
+        double currentSample;
 
-        // normalizing
-        samples[i/2] = currentSample*max;
+        // if the file has the double amount of samples, skip every other one
+        if (opts.half) {
+            currentSample = audioFile.samples[0][i*2];
+        } else {
+            currentSample = audioFile.samples[0][i];
+        }
+
+        // currentSample is between -1 and 1, normalizing
+        samples[i] = currentSample*max;
     }
 
     MidiFile mf;
-    //mf.addTrack(1);
     mf.setTicksPerQuarterNote(100);
 
     std::string name = "Example Whatev";
 
+    std::cout << "Generating..." << std::endl;
     for (int wave = 0; wave < 64; ++wave){
 
         std::vector<uchar> mm(410);
@@ -59,21 +113,20 @@ int main(void){
 
         mm[406] = 0x0; // Reserved
         mm[407] = 0x0; // Reserved
+
         int checksum = std::accumulate(
             mm.begin()+7, mm.end(), 0);
         mm[408] = checksum & 0x7f;
-        //mm[408] = 0x7f;
 
         mm[409] = 0xf7; // End
 
         mf.addEvent(0, 0, mm);
     }
 
-    //vector<uchar> clear = {0xff, 0x2f, 0x00};
-    //mf.addEvent(1, 200*64, clear);
-
     //mf.writeHex(std::cout);
-    mf.write("output.mid");
+    mf.write(opts.outfile);
+
+    std::cout << "Done!" << std::endl;
 
     return 0;
 }
